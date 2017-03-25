aaedab96b2a5ba14fdbe884483bd03468016db93
pratikpanchal131 <indianic@ind371.local>
2017-03-25 17:13:02 +0530

    Testing

---
+++ Example/Pods/Target Support Files/Pratiktest2/Pratiktest2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pratiktest2 : NSObject
+@end
+@implementation PodsDummy_Pratiktest2
+@end
---
+++ Pratiktest2/Classes/PKDatePicker.swift
@@ -0,0 +1,36 @@
+//
+//  PKDatePicker.swift
+//  Pods
+//
+//  Created by indianic on 25/03/17.
+//
+//
+
+import UIKit
+
+class PKDatePicker: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Welcome")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
